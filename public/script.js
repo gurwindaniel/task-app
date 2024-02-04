ded5c6ae00165d3312e75cf6e8ebcb3eb174c55f
gurwindaniel <gurwindaniel@gmail.com>
2024-02-04 23:36:13 +0530

    user submission hashed

--- public/script.js
+++ public/script.js
@@ -1,3 +1,53 @@
 $(document).ready(function(){
+
+    //Clearing form input
+
+    const clearInput=()=>{
+        $('input').val('')
+    }
+
+    const message=(message)=>{
+        if($("#message").children().length<1)
+        {
+            const parent=$("#message")
+        const child=$('<h5>').text(message)
+        child.css('color','red')
+        parent.append(child)
+
+        setTimeout(() => {
+            parent.empty()
+        }, 2000);
+
+        }
+
+        
+    }
+
+    const inputFetch=()=>{
+        const users={}
+        users.name=$("#myname").val()
+        users.email=$("#exampleInputEmail1").val()
+        users.password=$("#exampleInputPassword1").val()
+
+        return users
+    }
+    $("#usersub").on('click',function(e){
+        
+        const data = inputFetch()
     
+      
+        console.log(data)
+        $.ajax({
+            url:'/user',
+            method:'post',
+            type:'json',
+            data:data,
+            success:function(){
+                clearInput()
+               message("form submitted successfully")
+
+            }
+           })
+    })
+   
 })
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const router=express.Router()
+const bcrypt =require('bcryptjs')
 
 //sending response - Home page
 router.get('/',async(req,res)=>{
@@ -23,14 +24,22 @@ router.get('/',async(req,res)=>{
       
     })
 
+   
+//user route setup ->getting register page
+
+router.get('/register',async(req,res)=>{
+    res.render('register')
+})
 // Inser value to users
 router.post('/user',async(req,res)=>{
 
     const client =await pool.connect()
     const user=req.body
+    const hashed_password=await bcrypt.hash(user.password,10)
+    
     try{
 
-        await client.query('insert into users (name,email,password) values($1,$2,$3)',[user.name,user.email,user.password])
+        await client.query('insert into users (name,email,password) values($1,$2,$3)',[user.name,user.email,hashed_password])
 
         console.log(req.body)
         res.sendStatus(200)
